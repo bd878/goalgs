4ff11e4f0509ddce51f0d595d96cd84b93ea9486
bd878 <slavad878@gmail.com>
2023-09-22 12:02:29 +0300

    merge inplace with biton order

--- goalgs/merge/merge.go
+++ goalgs/merge/merge.go
@@ -18,3 +18,30 @@ func MergeAB(a []int, n int, b []int, m int) []int {
 
   return c
 }
+
+func Merge(a []int, l, m, r int) {
+  var i, j int
+  aux := make([]int, r-l)
+
+  for i = l; i < m; i++ {
+    aux[i] = a[i]
+  }
+  i = l
+
+  // reverse order
+  for j = m; j < r; j++ {
+    aux[(r-l)-(j-m)-1] = a[j]
+  }
+  j = r-1
+
+  for k := l; k < r; k++ {
+    // most right less than most left
+    if aux[j] < aux[i] {
+      a[k] = aux[j]
+      j = j-1
+    } else {
+      a[k] = aux[i]
+      i = i+1
+    }
+  }
+}
--- goalgs/merge/merge_test.go
+++ goalgs/merge/merge_test.go
@@ -8,9 +8,11 @@ import (
   "sort"
 )
 
+const MAX_N = 10e1
+
 var (
-  alen = flag.Int("a", 10e3, "a length")
-  blen = flag.Int("b", 10e2, "b length")
+  alen = flag.Int("alen", 10e3, "a length")
+  blen = flag.Int("blen", 10e2, "b length")
 )
 
 func TestMain(m *testing.M) {
@@ -20,14 +22,40 @@ func TestMain(m *testing.M) {
 }
 
 func TestMergeAB(t *testing.T) {
-  a := sort.IntSlice(rand.Perm(*alen))
-  b := sort.IntSlice(rand.Perm(*blen))
-
-  sort.Sort(a)
-  sort.Sort(b)
+  a, b := getSorted()
 
   c := MergeAB(a, a.Len(), b, b.Len())
   if !sort.IsSorted(sort.IntSlice(c)) {
     t.Error("c is not sorted")
   }
+}
+
+func TestMerge(t *testing.T) {
+  a, b := getSorted()
+
+  c := make([]int, 0, a.Len()+b.Len())
+  c = append(c, a...)
+  c = append(c, b...)
+
+  Merge(c, 0, a.Len(), a.Len()+b.Len())
+
+  if !sort.IsSorted(sort.IntSlice(c)) {
+    t.Error("c is not sorted")
+  }
+}
+
+func getSorted() (a, b sort.IntSlice) {
+  a = make([]int, *alen)
+  b = make([]int, *blen)
+  for i := 0; i < *alen; i++ {
+    a[i] = rand.Intn(MAX_N)
+  }
+  for i := 0; i < *blen; i++ {
+    b[i] = rand.Intn(MAX_N)
+  }
+
+  sort.Sort(a)
+  sort.Sort(b)
+
+  return a, b
 }
